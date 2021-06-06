eacd217e6ceae45ab55b2fdc9df999bff8e324f3
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2021-06-06 16:29:46 +0530

    Commit

---
+++ build/precache-manifest.c19a0195af537349d51867aea396ca50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5703bed242041b58c9a",
+    "url": "/static/css/main.7cabc36e.chunk.css"
+  },
+  {
+    "revision": "a5703bed242041b58c9a",
+    "url": "/static/js/main.a5703bed.chunk.js"
+  },
+  {
+    "revision": "c38de736b6bd9e2dfa61",
+    "url": "/static/css/1.7a53cf36.chunk.css"
+  },
+  {
+    "revision": "c38de736b6bd9e2dfa61",
+    "url": "/static/js/1.c38de736.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "ed188e14c2cae8805d1d956ae6f7f073",
+    "url": "/index.html"
+  }
+];
